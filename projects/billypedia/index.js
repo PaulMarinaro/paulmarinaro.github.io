3610d9b0fcaca9fbaf6f18db86fb15e5e6c35321
Paul Marinaro <paulmarinaro@gmail.com>
2018-04-12 14:32:25 +0000

    One Failing Test

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
     $.getJSON('data.json', function (data) {
         // YOUR CODE BELOW HERE //
-        
         $('#section-bio p:last-child').remove();
         
         $('#section-quotes').css('background-color', 'white').css('border-radius', '20px');
@@ -37,35 +36,51 @@ $(document).ready(function() {
         //     console.log(recording);
         // });
         
-        
-        // The append() method inserts specified content at the end of the selected elements.
-        // $(selector).append(content,function(index,html))
-        
-        // The appendTo() method inserts HTML elements at the end of the selected elements.
-        // $(content).appendTo(selector)
-        
-        // The text() method sets or returns the text content of the selected elements.
-        // Set text content: $(selector).text(content)
-        // Set text content using a function:
-        // $(selector).text(function(index,currentcontent))
-        
-        // The attr() method sets or returns attributes and values of the selected elements.
-        // Set multiple attributes and values:
-        //$(selector).attr({attribute:value, attribute:value,...}) 
-        
         // map the top rated recordings
         let topRated = data.discography.topRated;
         var topRatedAlbums = _.map(topRated, function(recording) {
             // console.log(recording);
             return recording;
         });
-        // display the topRatedAlbums as a list
-        topRatedAlbums.forEach(function(album){
-            $('#list-top-rated').append($('<li>' + '<i>title: </i>'  + album.title + '<br />' 
-            + ' <i>artist:<i> ' + album.artist + '<br />' 
-            + ' <i>release:<i> ' + album.release + '<br />'
-            + ' <i>year:<i> ' + album.year + '<br />' + '</li>'));
+        
+//_________________________________________________
+
+//   ($('#list-top-rated > li.top-rated').length) = 0 ???? should = 5
+
+// display the albums as a list
+        // make a function that creates a jQuery list item element
+        const $topRecordings = topRatedAlbums.map(function(album){
+            // turn album.title = Eastern Rebellion into eastern-rebellion.jpg
+            let albumTitle = album.title;
+            let albumTitleDashed = albumTitle.toLowerCase().replace(/ /gi, "-")
+            .replace(/\'/gi, "");
+            // make a jQuery list item element with class = 'recording'
+            let $listItem = $(`<li class="recording top-rated" id=${albumTitleDashed}>`)
+                .on('click', function(event){
+                    //  debugger;
+                    // expect($('#section-top-rated img').length).to.equal(1);
+                $topRatedImage.attr(`src`, `images/album/${albumTitleDashed}.jpg`);
+                // $topRatedImage.attr(`img id`, albumTitleDashed);
+            });
+            for (var key in album) {
+                // create jQuery <div> element, add class = key to each <div>, add text with key: value
+                const $element = $('<div>').addClass(key).text(`${key}: ${album[key]}`);
+                // add the element to the end of the $listItem
+                $listItem.append($element);
+            }
+            return $listItem;
         });
+        $('#list-top-rated').append($topRecordings);
+
+//_________________________________________________
+        // display the topRatedAlbums as a list
+        // topRatedAlbums.forEach(function(album){
+        //     $('#list-top-rated').append($('<li>' + 'title: '  + album.title + '<br />' 
+        //     + ' artist: ' + album.artist + '<br />' 
+        //     + ' release:<i> ' + album.release + '<br />'
+        //     + ' year:<i> ' + album.year + '<br />' + '</li>').addClass("recordings"));
+        // });
+//_________________________________________________
         
         // example of adding "Billy's Rider" to the page
         // make a <section> called 'section' with id = 'section-rider'
@@ -73,10 +88,11 @@ $(document).ready(function() {
         // insert <h3> at the end of "section" and insert text at the end of id="sections"
         // $section.append($('<h3>').text('Billy\'s Rider')).appendTo($('#sections'));
         
-        // make a <section> called 'section' with id = 'section-rider'
-        let $recordingSection = $('<section>').attr('id', 'section-recordings');
-        // $recordings.append($('<ul>').text('Billy\'s Rider')).appendTo($('#list-top-rated'));
-        $recordingSection.append($('<ul id="list-recordings" class="list-recordings">')).appendTo($('#list-top-rated'));
+        // make a <section> called 'recordingSection' with id = 'section-recordings'
+        let $recordingSection = $('<section>').attr('id', 'section-recordings')
+        .append($('<header id="header-recordings" class="header-recordings">Recordings</header>'));
+        //<header id="header-top-rated" class="header-recordings">Top Rated</header>
+        $recordingSection.append($('<ul id="list-recordings" class="list-recordings">')).appendTo($('#section-top-rated'));
 
         // map the recordings
         let recordings = data.discography.recordings;
@@ -85,54 +101,86 @@ $(document).ready(function() {
             return record;
         });
         
-        // <li class="recording">
         // display the albums as a list
+        // make a function that creates a jQuery list item element
         const $recordings = albums.map(function(album){
-            // console.log(album, 'album');
-            $('#list-recordings').append($('<li class = "recording" >'));
+            // turn album.title = Eastern Rebellion into eastern-rebellion.jpg
+            let albumTitle = album.title;
+            let albumTitleDashed = albumTitle.toLowerCase().replace(/ /gi, "-")
+            .replace(/\'/gi, "");
+            // make a jQuery list item element with class = 'recording'
+            let $listItem = $(`<li class = "recording" id=${albumTitleDashed}>`)
+                .on('click', function(event){
+                // console.log($(event.currentTarget));
+                $recordingImage.attr(`src`, `images/album/${albumTitleDashed}.jpg`);
+            });
             for (var key in album) {
-                // console.log(key, 'key');
-                // debugger;
-                // problem: it's appending for every element with a class = "recording"
-                return $('#list-recordings').append($('<div class = "key" + >' + key + ": "  
-                + album[key] + '</div>'));
+                // create jQuery <div> element, add class = key to each <div>, add text with key: value
+                const $element = $('<div>').addClass(key).text(`${key}: ${album[key]}`);
+                // add the element to the end of the $listItem
+                $listItem.append($element);
             }
+            return $listItem;
         });
+        $('#list-recordings').append($recordings);
 
-    // create function
-    // let $createRecording = function(album) {
-    //     // create template
-    //     // make a jQuery element that has this structure:
-    //     //<li class="recording">
-    //     let $listItem = $('<li class = "recording" >');
-    //         // <div class="title">Title: Eastern Rebellion</div>
-    //         let $title = $('<div class = "title" + >' + 'title: ' 
-    //         + album.title + '</div>');
-    //         // <div class="artist">Artist: Cedar Walton</div>
-    //         let $artist = $('<div class = "artist" + >' + 'artist: ' 
-    //         + album.artist + '</div>');           
-    //         // <div class="release">Release: Timeless</div>
-    //         let $release = $('<div class = "release" + >' + 'release: ' 
-    //         + album.release + '</div>');
-    //         // <div class="year">Year: 1976</div>
-    //         let $year = $('<div class = "year" + >' + 'year: ' + album.year + '</div>');
-    //     //</li>*/
+    // add an image to the top of the sections for top rated and recordings
+    
+    // add image to top of top-rated section
+    const $topRatedImageDiv = $('<div id="image-container-top-rated">').addClass("image-container");
+    const $topRatedImage = $('<img id="top-rated-image" src="images/album/voice-in-the-night.jpg" class="image">');
+    $topRatedImageDiv.append($topRatedImage);
+    $('#section-top-rated').prepend($topRatedImageDiv.append($topRatedImage));
+    
+    // add image to top of recording section
+    const $recordingImageDiv = $('<div id="image-container-recording">').addClass("image-container");
+    const $recordingImage = $('<img id="recording-image" src="images/album/eastern-rebellion.jpg" class="image">');
+    $recordingImageDiv.append($recordingImage);
+    $('#section-recordings').prepend($recordingImageDiv.append($recordingImage));
+    
+    
+    
+    // add a click handler to detect user clicks on image of Billy
+    // swap out image from data.images.billy
+    var i = 0;
+    const clickHandler = function(event) {
+        // console.log('clicked');
+        i++;
+        if (i === 4) i = 0;
+// ????????????????????????????????????????????????????????????????????????????        
+        // make image fade in? doesn't work...
+        $('#image-billy').attr(`src`, `images/billy/billy-${i}.jpg`).fadeIn(3000);
+    };
+    $('#image-container-billy').on('click', clickHandler);
+    
+    // Billy's Rider
+    let $rider = $('<section>').attr('id', 'section-rider');
+    $rider.append($('<h3>').text('Billy\'s Rider'))
+    .appendTo($('#sections'));
+    
+    var createTable = function(gear){
+        var createRow = function(item){
+            var $row = $("<tr>");
+            var $drum = $("<td>").text(item.drum);
+            var $brand = $("<td>").text(item.brand);
+            $row.append($drum);
+            $row.append($brand);
+            return $row;
+        }
+        var $table = $("<table>");
+        var $rows = gear.map(createRow);
+        $table.append($rows);
+        return $table;
+    };
+    let gear = [{drum: "Kick Drum", brand: "Tama"}, {drum: "Hi Hats", brand: "Paiste"},
+    {drum: "Snare", brand: "Tama"}, {drum: "Crash", brand: "Paiste"},
+    {drum: "High Tom", brand: "Tama"}, {drum: "Ride", brand: "Paiste"},
+    {drum: "Mid Tom", brand: "Tama"}, {drum: "Splash", brand: "Paiste"},
+    {drum: "Floor Tom", brand: "Tama"}, {drum: "Wood Block", brand: "Remo"},
+    {drum: "Timpani", brand: "Tama"}, {drum: "Gong", brand: "Paiste"},
+    {drum: "Throne", brand: "Tama"}, {drum: "Chimes", brand: "Paiste"},];
+    createTable(gear).appendTo($('#section-rider'));
         
-    // // return the template 
-    //     const $recording = $listItem.append($title).append($artist).append($release)
-    //     .append($year);
-    //     // const $recording = $title;
-    //     return $recording;
-    // };
-    // albums.forEach(function(album){
-    //     console.log($createRecording(album));
-    //     return $createRecording(album);
-    // });
-    
-    
-    
-    
-
         // YOUR CODE ABOVE HERE //
     })
     .fail(function() { console.log('getJSON on discography failed!'); });
